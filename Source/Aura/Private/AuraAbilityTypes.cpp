13d537e1403e57ace564f0ebfe2b68979a01ba19
unreal5 <lingbao@outlook.com>
2024-10-07 12:19:39 +0800

    159 mapping damage types to resistances 01:18

--- Source/Aura/Private/AuraAbilityTypes.cpp
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -7,5 +7,18 @@ UScriptStruct* FAuraGameplayEffectContext::GetScriptStruct() const
 
 bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
 {
-	return FGameplayEffectContext::NetSerialize(Ar, Map, bOutSuccess);
+	bool bRet = FGameplayEffectContext::NetSerialize(Ar, Map, bOutSuccess);
+	uint32 RepBits = 0;
+	if (Ar.IsSaving())
+	{
+		if (bIsBlockedHit) RepBits |= 1 << 0;
+		if (bIsCriticalHit) RepBits |= 1 << 1;
+	}
+	Ar.SerializeBits(&RepBits, 2);
+	if (Ar.IsLoading())
+	{
+		bIsBlockedHit = RepBits & (1 << 0);
+		bIsCriticalHit = RepBits & (1 << 1);
+	}
+	return bRet;
 }
--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -16,6 +16,18 @@ public:
 	void SetIsBlockedHit(bool IsBlockedHit) { bIsBlockedHit = IsBlockedHit; }
 
 	virtual UScriptStruct* GetScriptStruct() const override;
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	virtual FAuraGameplayEffectContext* Duplicate() const override
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
 	virtual bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
 
 protected:
@@ -24,4 +36,14 @@ protected:
 
 	UPROPERTY()
 	bool bIsCriticalHit = false;
+};
+
+template <>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true
+	};
 };
