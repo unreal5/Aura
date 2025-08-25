4997ed65fdcb303a993c0aea1f6de0e286536bc8
lbjtj <lbjtj@outlook.com>
2025-08-25 16:51:59 +0800

    0006. Player and Enemy Characters

---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,13 @@
+// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+};
