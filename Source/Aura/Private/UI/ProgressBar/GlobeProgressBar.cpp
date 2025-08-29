760434b2858ee1cf67eb94a9db7ac760577e03b3
lbjtj <lbjtj@outlook.com>
2025-08-29 10:23:21 +0800

    0031. Health Globe

---
+++ Source/Aura/Private/UI/ProgressBar/GlobeProgressBar.cpp
@@ -0,0 +1,82 @@
+// 版权没有，拷贝自由。
+
+
+#include "UI/ProgressBar/GlobeProgressBar.h"
+
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/Image.h"
+#include "Components/OverlaySlot.h"
+#include "Components/ProgressBar.h"
+#include "Components/SizeBox.h"
+
+void UGlobeProgressBar::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	UpdateSizeBox();
+	UpdateBackground();
+	SetGlobeImage();
+	UpdateGlobePadding();
+	UpdateGlassBrush();
+	UpdateGlassPadding();
+}
+
+void UGlobeProgressBar::UpdateSizeBox() const
+{
+	if (SizeBox_Root)
+	{
+		SizeBox_Root->SetWidthOverride(BoxWidth);
+		SizeBox_Root->SetHeightOverride(BoxHeight);
+	}
+}
+
+void UGlobeProgressBar::UpdateBackground() const
+{
+	if (Image_Background)
+	{
+		Image_Background->SetBrush(BackgroundBrush);
+	}
+}
+
+void UGlobeProgressBar::SetGlobeImage() const
+{
+	FProgressBarStyle NewStyle;
+	NewStyle.BackgroundImage.TintColor = FSlateColor(FColor{1, 1, 1, 0}); //透明
+	NewStyle.FillImage = ProgressBarFillImage;
+	if (ProgressBar_Globe)
+	{
+		ProgressBar_Globe->SetWidgetStyle(NewStyle);
+	}
+}
+
+void UGlobeProgressBar::UpdateGlobePadding() const
+{
+	FMargin NewPadding{GlobePadding};
+	if (ProgressBar_Globe)
+	{
+		if (auto ProgressBar_Slot = UWidgetLayoutLibrary::SlotAsOverlaySlot(ProgressBar_Globe))
+		{
+			ProgressBar_Slot->SetPadding(NewPadding);
+		}
+	}
+}
+
+void UGlobeProgressBar::UpdateGlassBrush() const
+{
+	if (Image_Glass)
+	{
+		Image_Glass->SetBrush(GlassBrush);
+	}
+}
+
+void UGlobeProgressBar::UpdateGlassPadding() const
+{
+	FMargin NewPadding{GlobePadding};
+	if (Image_Glass)
+	{
+		if (auto Glass_Slot = UWidgetLayoutLibrary::SlotAsOverlaySlot(Image_Glass))
+		{
+			Glass_Slot->SetPadding(NewPadding);
+		}
+		Image_Glass->SetColorAndOpacity(FLinearColor{1, 1, 1, 0.5f});
+	}
+}
---
+++ Source/Aura/Public/UI/ProgressBar/GlobeProgressBar.h
@@ -0,0 +1,63 @@
+// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/AuraUserWidget.h"
+#include "GlobeProgressBar.generated.h"
+
+class UProgressBar;
+class UImage;
+class USizeBox;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UGlobeProgressBar : public UAuraUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativePreConstruct() override;
+
+	// SizeBox
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Globe属性")
+	float BoxWidth = 256.f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Globe属性")
+	float BoxHeight = 256.f;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<USizeBox> SizeBox_Root;
+
+	// Background
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UImage> Image_Background;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Globe属性")
+	FSlateBrush BackgroundBrush;
+
+	// ProgressBar
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UProgressBar> ProgressBar_Globe;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Globe属性")
+	FSlateBrush ProgressBarFillImage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Globe属性")
+	float GlobePadding = 10.f;
+
+	// Glass
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Globe属性", meta=(BindWidget))
+	TObjectPtr<UImage> Image_Glass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Globe属性")
+	FSlateBrush GlassBrush;
+
+private:
+	void UpdateSizeBox() const;
+	void UpdateBackground() const;
+	void SetGlobeImage() const;
+	void UpdateGlobePadding() const;
+	void UpdateGlassBrush() const;
+	void UpdateGlassPadding() const;
+};
