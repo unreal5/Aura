f73253cbe6e0ff972c6fb9386e4440cdfc562cd1
lbjtj <lbjtj@outlook.com>
2025-09-08 17:23:52 +0800

    0060. Message Widget

---
+++ Source/Aura/Private/UI/Overlay/SubWidget/EffectMessage.cpp
@@ -0,0 +1,35 @@
+// 版权没有，拷贝自由。
+
+
+#include "UI/Overlay/SubWidget/EffectMessage.h"
+
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void UEffectMessage::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	
+}
+
+void UEffectMessage::SetMessageText(const FText& InText)
+{
+	if (Text_Message)
+	{
+		Text_Message->SetText(InText);
+	}
+}
+
+void UEffectMessage::SetMessageIcon(UTexture2D* InIcon)
+{
+	if (Image_Icon)
+	{
+		Image_Icon->SetBrushFromTexture(InIcon);
+		FSlateBrush Brush;
+		Brush.DrawAs = ESlateBrushDrawType::Image;
+		Brush.Tiling = ESlateBrushTileType::NoTile;
+		Brush.ImageSize = ImageSize;
+		Brush.SetResourceObject(InIcon);
+		Image_Icon->SetBrush(Brush);
+	}
+}
---
+++ Source/Aura/Public/UI/Overlay/SubWidget/EffectMessage.h
@@ -0,0 +1,34 @@
+// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/AuraUserWidget.h"
+#include "EffectMessage.generated.h"
+
+class UTextBlock;
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UEffectMessage : public UAuraUserWidget
+{
+	GENERATED_BODY()
+protected:
+	virtual void NativePreConstruct() override;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	UImage* Image_Icon;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	UTextBlock* Text_Message;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FVector2D ImageSize = FVector2D(64, 64);
+	
+	UFUNCTION(BlueprintCallable)
+	void SetMessageText(const FText& InText);
+	UFUNCTION(BlueprintCallable)
+	void SetMessageIcon(UTexture2D* InIcon);
+
+};
