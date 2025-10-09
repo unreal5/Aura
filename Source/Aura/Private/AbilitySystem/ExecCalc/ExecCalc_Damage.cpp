af7e4ea4ac125c5b7581cef10a890e1f8764a176
lbjtj <lbjtj@outlook.com>
2025-10-09 17:34:08 +0800

    0144. ExecCalcs - Capturing Attributes

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,54 @@
+// 版权没有，拷贝自由。
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+// 辅助结构体
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	AuraDamageStatics();
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics DmgStatics;
+	return DmgStatics;
+}
+
+AuraDamageStatics::AuraDamageStatics()
+{
+	DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+}
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+                                              FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	auto SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	auto TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	auto SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	auto TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	auto&& Spec = ExecutionParams.GetOwningSpec();
+
+	FAggregatorEvaluateParameters EvalParams;
+	EvalParams.SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	EvalParams.TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	float Armor = 0.f;
+	bool bCapturedArmor = ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(
+		DamageStatics().ArmorDef, EvalParams, Armor);
+	// ExecutionCalculate 不会执行PreAttribute/PreBaseAttribute clamp操作，如果需要请自行处理
+	Armor += 100.f;
+	FGameplayModifierEvaluatedData ModifierData{DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor};
+	OutExecutionOutput.AddOutputModifier(ModifierData);
+}
