283c0fc10efaaf4f4c511f1a37844ee28ad1007d
lbjtj <lbjtj@outlook.com>
2026-05-29 12:02:45 +0800

    0045 Infinite Effect Application

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -4,6 +4,7 @@
 #include "Actor/AuraEffectActor.h"
 
 #include "AbilitySystemComponent.h"
+#include "Engine/World.h"
 
 
 AAuraEffectActor::AAuraEffectActor()
@@ -15,10 +16,6 @@ AAuraEffectActor::AAuraEffectActor()
 	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
-void AAuraEffectActor::BeginPlay()
-{
-	Super::BeginPlay();
-}
 
 bool AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> InGameplayEffectClass)
 {
@@ -36,4 +33,92 @@ bool AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 		return ActivateHandle.WasSuccessfullyApplied();
 	}
 	return false;
+}
+
+void AAuraEffectActor::OnBeginOverlap(AActor* TargetActor)
+{
+	if (IsValid(InstantGameplayEffectClass) &&
+		InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+
+		if (bDestroyOnEffectRemoval) 
+			Destroy();
+	}
+
+	if (IsValid(DurationGameplayEffectClass) &&
+		DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+		
+		if (bDestroyOnEffectRemoval) 
+			Destroy();
+	}
+
+	if (IsValid(InfiniteGameplayEffectClass) &&
+		InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+		
+		if (bDestroyOnEffectRemoval) 
+			Destroy();
+	}
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if (IsValid(InstantGameplayEffectClass) && InstantEffectApplicationPolicy ==
+		EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+		
+		if (bDestroyOnEffectRemoval) 
+			Destroy();
+	}
+
+	if (IsValid(DurationGameplayEffectClass) &&
+		DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+		
+		if (bDestroyOnEffectRemoval) 
+			Destroy();
+	}
+
+	if (IsValid(InfiniteGameplayEffectClass) &&
+		InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+		
+		if (bDestroyOnEffectRemoval) 
+			Destroy();
+	}
+	// 结束重叠时，按要求移除无限效果
+	if (IsValid(InfiniteGameplayEffectClass) &&
+		InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemovalOnEndOverlap)
+	{
+		auto TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+		if (TargetASC)
+		{
+			TargetASC->RemoveActiveGameplayEffectBySourceEffect(InfiniteGameplayEffectClass, TargetASC, 1);
+			/*
+			auto ActiveEffectHandles = TargetASC->GetActiveEffects(FGameplayEffectQuery());
+			const UWorld* World = GetWorld();
+			const float WorldTime = IsValid(World) ? World->GetTimeSeconds() : 0.f;
+			for (const FActiveGameplayEffectHandle& ActiveEffectHandle : ActiveEffectHandles)
+			{
+				const FActiveGameplayEffect* ActiveEffect = TargetASC->GetActiveGameplayEffect(ActiveEffectHandle);
+				if (!ActiveEffect) continue;
+				
+				const FGameplayEffectSpec& Spec = ActiveEffect->Spec;
+				const UGameplayEffect* EffectDef = TargetASC->GetGameplayEffectDefForHandle(ActiveEffectHandle);
+				const FString EffectName = GetNameSafe(EffectDef);
+				const int32 StackCount = TargetASC->GetCurrentStackCount(ActiveEffectHandle);
+				const float Duration = Spec.GetDuration();
+				const float RemainingTime = IsValid(World) ? ActiveEffect->GetTimeRemaining(WorldTime) : Duration;
+				const float Period = Spec.GetPeriod();
+			}
+			*/
+		}
+	}
 }
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -9,6 +9,21 @@
 
 class UGameplayEffect;
 
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy : uint8
+{
+	ApplyOnOverlap,
+	ApplyOnEndOverlap,
+	DoNotApply,
+};
+
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy : uint8
+{
+	RemovalOnEndOverlap,
+	DoNotRemove,
+};
+
 UCLASS()
 class AURA_API AAuraEffectActor : public AActor
 {
@@ -18,14 +33,31 @@ public:
 	AAuraEffectActor();
 
 protected:
-	void BeginPlay() override;
-
 	UFUNCTION(BlueprintCallable, meta = (ExpandBoolAsExecs = "ReturnValue"))
 	bool ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> InGameplayEffectClass);
 
+	UFUNCTION(BlueprintCallable, Category = "Aura")
+	void OnBeginOverlap(AActor* TargetActor);
+	UFUNCTION(BlueprintCallable, Category = "Aura")
+	void OnEndOverlap(AActor* TargetActor);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura | Effect")
+	bool bDestroyOnEffectRemoval = false;
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura | Effect | Instant")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura | Effect | Instant")
+	EEffectApplicationPolicy InstantEffectApplicationPolicy = EEffectApplicationPolicy::ApplyOnOverlap;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura | Effect | Duration")
 	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura | Effect | Duration")
+	EEffectApplicationPolicy DurationEffectApplicationPolicy = EEffectApplicationPolicy::ApplyOnOverlap;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura | Effect | Infinite")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura | Effect | Infinite")
+	EEffectApplicationPolicy InfiniteEffectApplicationPolicy;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura | Effect | Infinite")
+	EEffectRemovalPolicy InfiniteEffectRemovalPolicy = EEffectRemovalPolicy::RemovalOnEndOverlap;
 };
