4b61cebc916e048e0b2d66098d20e54aed440245
lbjtj <lbjtj@outlook.com>
2026-06-04 11:34:25 +0800

    0061 Animating the message widget

---
+++ Source/Aura/Private/UI/Overlay/SubWidget/EffectMessage.cpp
@@ -0,0 +1,25 @@
+﻿// 版权没有，拷贝自由。
+
+
+#include "UI/Overlay/SubWidget/EffectMessage.h"
+
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Engine/Texture2D.h"
+
+void UEffectMessage::SetImageAndText(const FText& InText, UTexture2D* InTexture)
+{
+	if (MessageText)
+	{
+		MessageText->SetText(InText);
+	}
+	
+	if (MessageImage && InTexture)
+	{
+		FSlateBrush Brush;
+		Brush.SetResourceObject(InTexture);
+		Brush.SetImageSize(ImageSize);
+		MessageImage->SetBrush(Brush);
+	}
+	PlayAnimationAndDestroy();
+}
---
+++ Source/Aura/Public/UI/Overlay/SubWidget/EffectMessage.h
@@ -0,0 +1,28 @@
+﻿// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/AuraUserWidget.h"
+#include "EffectMessage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UEffectMessage : public UAuraUserWidget
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<class UTextBlock> MessageText;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<class UImage> MessageImage;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FVector2D ImageSize = FVector2D(75.f, 75.f);
+	
+	UFUNCTION(BlueprintCallable, Category = "Aura | EffectMessage")
+	void SetImageAndText(const FText& InText, UTexture2D* InTexture);
+	UFUNCTION(BlueprintImplementableEvent, Category = "Aura | EffectMessage")
+	void PlayAnimationAndDestroy();
+};
