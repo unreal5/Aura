bcff009b56e34e1f4f8f2e12a30ae3972174956a
lbjtj <lbjtj@outlook.com>
2026-06-08 08:45:38 +0800

    0077 Attribute menu - frame value 05:18 n/a

---
+++ Source/Aura/Private/UI/AttributeMenu/FrameValue.cpp
@@ -0,0 +1,23 @@
+﻿// 版权没有，拷贝自由。
+
+
+#include "UI/AttributeMenu/FrameValue.h"
+
+#include "Components/SizeBox.h"
+
+
+void UFrameValue::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	UpdateFrameSize();
+}
+
+void UFrameValue::UpdateFrameSize()
+{
+	if (SizeBox_Root)
+	{
+		SizeBox_Root->SetWidthOverride(BoxWidth);
+		SizeBox_Root->SetHeightOverride(BoxHeight);
+	}
+}
---
+++ Source/Aura/Public/UI/AttributeMenu/FrameValue.h
@@ -0,0 +1,31 @@
+﻿// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/AuraUserWidget.h"
+#include "FrameValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UFrameValue : public UAuraUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	void NativePreConstruct() override;
+
+public:
+	// SizeBox_Root是一个SizeBox组件，用于控制框架的大小。通过编辑BoxWidth和BoxHeight属性，可以调整框架的宽度和高度。
+	UPROPERTY(meta = (BindWidget))
+	class USizeBox* SizeBox_Root;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FrameProperties")
+	float BoxWidth = 80.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FrameProperties")
+	float BoxHeight = 45.f;
+
+private:
+	void UpdateFrameSize();
+};
