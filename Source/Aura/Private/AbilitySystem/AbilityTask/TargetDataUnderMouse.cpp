7bad0c9cb5f791176ac08f04b88f21f4c31cf265
lbjtj <lbjtj@outlook.com>
2026-06-17 17:37:52 +0800

    0115. Custom Ability Tasks

---
+++ Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
@@ -0,0 +1,25 @@
+﻿// 版权没有，拷贝自由。
+
+
+#include "AbilitySystem/AbilityTask/TargetDataUnderMouse.h"
+
+#include "AbilitySystemComponent.h"
+#include "GameFramework/PlayerController.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility,
+                                                                         FName TaskInstanceName)
+{
+	return NewAbilityTask<UTargetDataUnderMouse>(OwningAbility, TaskInstanceName);
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	// Super::Activate();
+	auto PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	if (!PC) return;
+
+	FHitResult HitResult;
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, HitResult);
+	// 这里为什么不判断没有命中？因为没有命中的话，HitResult.Location就是一个无效的值，广播出去也没什么关系。
+	OnTargetDataReady.Broadcast(HitResult.Location);
+}
---
+++ Source/Aura/Public/AbilitySystem/AbilityTask/TargetDataUnderMouse.h
@@ -0,0 +1,29 @@
+﻿// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "TargetDataUnderMouse.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataDelegate, const FVector&, TargetData);
+
+UCLASS()
+class AURA_API UTargetDataUnderMouse : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks",
+		meta = (DisplayName = "Target Data Under Mouse", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility",
+			BlueprintInternalUseOnly = "TRUE"))
+	static UTargetDataUnderMouse* CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility,
+	                                                         FName TaskInstanceName = FName("TargetDataUnderMouse"));
+
+	UPROPERTY(BlueprintAssignable)
+	FMouseTargetDataDelegate OnTargetDataReady;
+
+protected:
+	void Activate() override;
+};
