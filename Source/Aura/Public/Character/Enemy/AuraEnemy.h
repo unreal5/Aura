53c60a8365ec749f9bb61ca7b1d131a84225427a
lbjtj <lbjtj@outlook.com>
2026-06-29 18:10:36 +0800

    0140. Floating Text Widget

--- Source/Aura/Public/Character/Enemy/AuraEnemy.h
+++ Source/Aura/Public/Character/Enemy/AuraEnemy.h
@@ -24,12 +24,14 @@ public:
 	FOnAttributeValueChangedWithTagSignature OnAttributeValueChangedWithTag;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="战斗")
-	float Lifespan = 50.f;
+	float Lifespan = 5.f;
 	
 	/* 实现ICombatInterface*/
 	int32 GetPlayerLevel_Implementation() const override;
 	void Die_Implementation() override;
 	
+	void HighlightActor_Implementation() override;
+	void UnHighlightActor_Implementation() override;	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="战斗")
 	bool bHitReacting = false;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="移动")
@@ -38,8 +40,7 @@ protected:
 	void InitAbilityActorInfo() override;
 	void InitializeDefaultAttributes() const override;
 private:
-	void HighlightActor_Implementation() override;
-	void UnHighlightActor_Implementation() override;
+
 
 	//对于敌人，数据只存在于服务器上，客户端不需要知道，所以不复制。
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="角色默认", meta=(AllowPrivateAccess=true))
---
+++ Source/Aura/Public/UI/Widget/DamageTextComponent.h
@@ -0,0 +1,17 @@
+﻿// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "DamageTextComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AURA_API UDamageTextComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category="DamageText")
+	void SetDamageText(float DamageAmount);
+};
